35c51063bdfd551026e437d943d8156ec552f598
layeghimahsa <mahsalayeghi@cmail.carleton.ca>
2021-03-30 18:02:21 -0700

    adding more actions

--- src/java/Action.java
+++ src/java/Action.java
@@ -5,6 +5,9 @@ class Action {
 	private static final String DASH_PATTERN = "^dash\\(-?[0-9|\\.]+\\)$";
 	private static final String KICK_PATTERN = "^kick\\(-?[0-9|\\.]+,-?[0-9|\\.]+\\)$";
 	private static final String MOVE_PATTERN = "^move\\(-?[0-9|\\.]+,-?[0-9|\\.]+\\)$";
+	private static final String CATCH_PATTERN = "^catch\\(-?[0-9]+\\)$";
+	private static final String CHANGEVIEW_PATTERN = "^change_view\\([narrow|normal|wide],[high|low]\\)$";
+	
 
 	
     static void execute(Player player, String action) {
@@ -18,6 +21,11 @@ class Action {
 		} else if (Pattern.matches(MOVE_PATTERN, action)) {
 			player.move(Double.parseDouble(action.substring(5, action.indexOf(","))), 
 					Double.parseDouble(action.substring(action.indexOf(",")+1, action.length()-1)));
-		}
+		} else if (Pattern.matches(CATCH_PATTERN, action)) {
+			player.catch_ball(Double.parseDouble(action.substring(6, action.length()-1)));
+		} else if (Pattern.matches(CHANGEVIEW_PATTERN, action)){
+			player.changeView(action.substring(12, action.indexOf(",")), 
+					action.substring(action.indexOf(",")+1, action.length()-1));
+		} 
     }
 }
